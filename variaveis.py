a6357560ba0a70bbed0f7a3e3167231bf8bfc47f
Th4Hawk <whitereaper7777@protonmail.com>
2023-11-11 20:03:13 -0300

    Olá mundo e variaveis

---
+++ variaveis.py
@@ -0,0 +1,25 @@
+# Declarando uma variavel do tipo string
+nome = "hawk"
+# Declarando uma variavel do tipo int
+numero = 10
+# Declarando uma variavel do tipo float
+numero2 = 1.2 
+# Declarando uma variavel do tipo boolean
+verificado = True 
+# Declarando uma sequencia de valores mutavel
+valores1 = [1,2,3,4,5]
+# Declarando uma sequencia de valores imutavel
+valores2 = (6,7,8,9,10)
+# Criando uma coleção de dados
+coleção = {"nome": "blackout", "idade": 16, "estado": "SP"}
+# Criando um conjunto
+conjunto = {11,12,13,22,14}
+# Acessando o valor de uma variavel
+print("O valor de numero é:", numero)
+print("O valor da string é", nome)
+print("O valor do float é:", numero2)
+print("O valor do booleano é:", verificado)
+print("O valor da sequencia mutavel é", valores1)
+print("O valor da sequencia imutavel é", valores2)
+print("Os dados que estão na coleção são:", coleção)
+print("Os valores que estão no conjuto são:", conjunto)
