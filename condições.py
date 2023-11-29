6eeca9b450b70c30e9f5c45f6164f47f67bcf940
Th4Hawk <whitereaper7777@protonmail.com>
2023-11-29 19:44:39 -0300

    Pequena organização

---
+++ condições.py
@@ -0,0 +1,22 @@
+numero = 1
+if numero == 1:
+    print("Numero é igual a 1")
+elif numero == 2:
+    print("Numero é igual a 2")
+else:
+    print("Numero invalido")
+
+lista1 = [13,22,17]
+lista2 = [13,22,17]
+
+if lista1 is lista2:
+    print("As variaveis são o mesmo objeto")
+else:
+    print("As variaveis não são o mesmo objeto")
+
+lista = [4,2,5]
+
+if 4 in lista: 
+    print("O numero 4 esta presente na lista")
+else:
+    print("O numero 4 não esta preente na lista")
---
+++ loops.py
@@ -0,0 +1,10 @@
+# loop while
+contador = 0
+print("Resultados do loop while")
+while contador < 5:
+    print(contador)
+    contador += 1
+# loop for
+print("Resultados do loop for")
+for i in range(10):
+    print(i)
